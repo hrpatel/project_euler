943b457e71bb62994e67ee735849120b4aa83d12
hrpatel <pate3900+github@gmail.com>
2015-09-27 22:50:55 -0400

    commenting/clean up

--- euler_003.py
+++ euler_003.py
@@ -13,12 +13,17 @@ for i in range(num_entries):
 
 # find all prime numbers
 def get_prime(n):
-    max_int = int(sqrt(n))
+    # initialize iterator, array, upper test limit
     itr = 2
     primes = []
+    max_int = int(sqrt(n))
+
+    # loop through each number in ascending order
     while itr <= max_int:
+        # we're done
         if n == 1:
             break
+        # we found a factor, start over
         if n % itr == 0:
             primes.append(itr)
             n /= itr
@@ -26,10 +31,12 @@ def get_prime(n):
         else:
             itr += 1
 
+    # attach left over prime (1 is valid)
     primes.append(n)
 
     return primes
 
 # iterate through each
 for num in inputs:
+    # find max and print
     print max([x for x in get_prime(num)])
