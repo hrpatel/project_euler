76e5fd8dd402f9106e4981f31f1d490066913549
hrpatel <pate3900+github@gmail.com>
2015-10-03 12:59:26 -0400

    fix palindrome function, new strategy - precompute palindromes - find closes match in input

--- euler_004.py
+++ euler_004.py
@@ -1,5 +1,3 @@
-from math import sqrt
-
 __author__ = 'ray'
 
 # get number of inputs
@@ -23,17 +21,37 @@ def palindrome(test):
     elif len(test) == 2:
         return test[0] == test[1]
     else:
-        return palindrome(test[1:-1])
+        if test[0] == test[-1]:
+            return palindrome(test[1:-1])
+        else:
+            return False
 
-print(palindrome("12345"))
 
-exit()
+def main():
 
-# iterate through each
-for num in inputs:
-    # generate a product from 3 digit integers and check if the results are prime
+    # generate all 6 digit palindromes that are a product of 3 digit integers
+    palindromes = set()
     for x in xrange(100,1000):
         for y in xrange(100,1000):
             product = x * y
-            if product < num and palindrome(str(product)):
-                print(product)
+            if product > 100000 and palindrome(str(product)):
+                palindromes.add(product)
+
+    # make a sorted list of palindromes
+    l = list(palindromes)
+    l.sort()
+
+    # iterate through each input
+    for num in inputs:
+        # input constraints
+        if num < 101102 or num > 999999:
+            continue
+
+        # find the closest palindrome to the input
+        for val in xrange(len(l)):
+            if l[val] > num:
+                print l[val-1]
+                break
+
+
+main()
